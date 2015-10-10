ed3376976b20794439505dd5db15753b569049e6
nathanchance <natechancellor@gmail..com>
2015-10-09 18:43:51 -0700

    Displaying info
    Display the information input back to the user

--- Registration.java
+++ Registration.java
@@ -1,30 +1,13 @@
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Registration {
   /////////////////////////////////////////////////////////////////
   // Main program
   /////////////////////////////////////////////////////////////////
   public static void main(String[] args) {
-    /////////////////////////////////////////////////////////////////
-    // data variables
-    /////////////////////////////////////////////////////////////////
-    String firstName = ""; // first name of person
-    String lastName = ""; // last name of person
-    String emailAddress = ""; // email address of person
-    String password = ""; // password of person
-    String[] type = {"Patient", "Doctor", "Nurse", "Healthcare Provider", "Lab Staff", "Pharmacist"}; // types that person could be
-    String[] months = new String[12]; // months array (1-12)
-    for (int i = 0; i < 12; i++) // fill the array automatically
-      months[i] = Integer.toString(i+1);
-    String[] days = new String[31]; // days array (1-31)
-    for (int i = 0; i < 31; i++) // fill the array automatically
-      days[i] = Integer.toString(i+1);
-    String[] years = new String[125]; // years array (1900-2025)
-    for (int i = 0; i < 125; i++) // fill the array automatically
-      years[i] = Integer.toString(2025-i);
-    int registrationCode = 0; // registration code if person is an internal user of the program (everything not patient)
     /////////////////////////////////////////////////////////////////
     // GUI variables
     /////////////////////////////////////////////////////////////////
@@ -48,9 +31,20 @@ public class Registration {
     /////////////////////////////////////////////////////////////////
     // Combo boxes
     /////////////////////////////////////////////////////////////////
+    String[] months = new String[12]; // months array (1-12)
+    for (int i = 0; i < 12; i++) // fill the array automatically
+      months[i] = Integer.toString(i+1);
+    String[] days = new String[31]; // days array (1-31)
+    for (int i = 0; i < 31; i++) // fill the array automatically
+      days[i] = Integer.toString(i+1);
+    String[] years = new String[125]; // years array (1900-2025)
+    for (int i = 0; i < 125; i++) // fill the array automatically
+      years[i] = Integer.toString(2025-i);
     JComboBox monthDropdown = new JComboBox<String>(months); // DOB month dropdown
-    JComboBox daysDropdown = new JComboBox<String>(days); // DOB day dropdown
-    JComboBox yearsDropdown = new JComboBox<String>(years); // DOB year dropdown
+    JComboBox dayDropdown = new JComboBox<String>(days); // DOB day dropdown
+    JComboBox yearDropdown = new JComboBox<String>(years); // DOB year dropdown
+
+    String[] type = {"Patient", "Doctor", "Nurse", "Healthcare Provider", "Lab Staff", "Pharmacist"}; // types that person could be
     JComboBox typeDropdown = new JComboBox<String>(type); // user type dropdown
     /////////////////////////////////////////////////////////////////
     // Buttons
@@ -78,8 +72,8 @@ public class Registration {
     JPanel dobPanel = new JPanel();
     dobPanel.add(dobLabel);
     dobPanel.add(monthDropdown);
-    dobPanel.add(daysDropdown);
-    dobPanel.add(yearsDropdown);
+    dobPanel.add(dayDropdown);
+    dobPanel.add(yearDropdown);
 
     JPanel typePanel = new JPanel();
     typePanel.add(typeLabel);
@@ -99,6 +93,35 @@ public class Registration {
     content.add(dobPanel);
     content.add(typePanel);
     content.add(submitPanel);
+
+    submitButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+          /////////////////////////////////////////////////////////////////
+          // data variables for the user
+          /////////////////////////////////////////////////////////////////
+          String firstName = ""; // first name of person
+          String lastName = ""; // last name of person
+          String emailAddress = ""; // email address of person
+          String password = ""; // password of person
+          int month = 0; // month of DOB
+          int day = 0; // day of DOB
+          int year = 0; // year of DOB
+          int registrationCode = 0; // registration code if person is an internal user of the program (everything not patient)
+
+          firstName = firstNameField.getText();
+          lastName = lastNameField.getText();
+          emailAddress = emailField.getText();
+          password = passwordField.getText();
+          month = Integer.parseInt((String)monthDropdown.getSelectedItem());
+          day = Integer.parseInt((String)dayDropdown.getSelectedItem());
+          year = Integer.parseInt((String)yearDropdown.getSelectedItem());
+
+          String dob = "" + month + "/" + day + "/" + year;
+          String info = "First Name: " + firstName + "\nLast Name: " + lastName + "\nDate of Birth: " + dob + "\nEmail: " + emailAddress + "\nPassword: " + password + "\nType: " + typeDropdown.getSelectedItem();
+		      // default title and icon
+		      JOptionPane.showMessageDialog(frame, info);
+        }
+    });
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(500,250);
