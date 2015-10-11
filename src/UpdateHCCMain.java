5457381ddab5d877ffb3b4347178d06c37744113
timothymillea <timothymillea@10.0.1.16>
2015-10-11 13:31:32 -0700

    Skeleton code outline

--- src/UpdateHCCMain.java
+++ src/UpdateHCCMain.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 public class UpdateHCCMain {
+	public static void main(String[] args){
 	JFrame frame = new JFrame();
 	UpdateHCFrame hcframe = new UpdateHCFrame();
-	
+	frame.setSize(500, 300);
+	frame.add(hcframe);
+	frame.setVisible(true);
+	}
 }
--- src/UpdateHCFrame.java
+++ src/UpdateHCFrame.java
@@ -1,7 +1,37 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 public class UpdateHCFrame extends JPanel{
 	public UpdateHCFrame (){
-		super(GridLayout);
+		super();
+		JTextField conditions = new JTextField();
+		JButton edit = new JButton("Edit");
+		this.add(conditions);
+		this.add(edit);
+	}
+	
+	public class ButtonListener implements ActionListener{ 
+		public void actionPerformed(ActionEvent e){
+			
+		}
+	}
+	
+	public class HCListener implements DocumentListener{
+		public void changedUpdate(DocumentEvent e){
+			
+		}
 		
+		public void insertUpdate(DocumentEvent e){
+			
+		}
+		
+		public void removeUpdate(DocumentEvent e){
+			
+		}
 	}
 }
