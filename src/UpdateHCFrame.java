7d83e528c4703d871e2ce1f886b164ef27b0d276
Kirik0 <mkkim6@asu.edu>
2015-10-11 21:26:48 -0700

    Update UpdateHCFrame.java

--- src/UpdateHCFrame.java
+++ src/UpdateHCFrame.java
@@ -11,13 +11,19 @@ public class UpdateHCFrame extends JPanel{
 	JTextArea conditions;
 	JButton edit;
 	JButton save;
+	JTextField nameField;
+    	JTextField DOBField;
 	String filename;
 	Writer writer;
 	public UpdateHCFrame () throws IOException{
 		super();
+		nameField=new JTextField("Patient Name",20);
+        	DOBField=new JTextField("Date of Birth: mm/dd/yyyy",20);
 		conditions = new JTextArea("Health Care Conditions",20, 20);
 		JButton edit = new JButton("Edit");
 		JButton save = new JButton("Save");
+		this.add(nameField);
+        	this.add(DOBField);
 		this.add(conditions);
 		this.add(edit);
 		this.add(save);
