fe5be1604c463cd5538307a8372230789027246f
drschrod <dylan.schroder31@gmail.com>
2015-10-11 21:28:01 -0700

    GUI update
    added two textfields for patient name and DOB

--- src/UpdateHCFrame.java
+++ src/UpdateHCFrame.java
@@ -7,17 +7,25 @@ import javax.swing.*;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+
 public class UpdateHCFrame extends JPanel{
 	JTextArea conditions;
 	JButton edit;
 	JButton save;
+	JTextField nameField;
+	JTextField DOBField;
 	String filename;
 	Writer writer;
+	
 	public UpdateHCFrame () throws IOException{
 		super();
+		nameField=new JTextField("Patient Name",20);
+		DOBField=new JTextField("Date of Birth: mm/dd/yyyy",20);
 		conditions = new JTextArea("Health Care Conditions",20, 20);
 		JButton edit = new JButton("Edit");
 		JButton save = new JButton("Save");
+		this.add(nameField);
+		this.add(DOBField);
 		this.add(conditions);
 		this.add(edit);
 		this.add(save);
@@ -38,6 +46,9 @@ public class UpdateHCFrame extends JPanel{
 		public void actionPerformed(ActionEvent e){
 			try {
 				writer = new BufferedWriter(new FileWriter(filename));
+				
+				//conditions.write(writer);
+				
 			} catch (IOException e2) {
 				// TODO Auto-generated catch block
 				e2.printStackTrace();
