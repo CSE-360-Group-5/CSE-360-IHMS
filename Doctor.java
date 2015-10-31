996cb558c13f9548b9119bcca88e585bbeb8e347
timothymillea <timothymillea@timothys-mbp.mobile.asu.edu>
2015-10-31 14:52:13 -0700

    Added rough outline of Doctor Services

--- Doctor.java
+++ Doctor.java
@@ -8,5 +8,21 @@ public class Doctor {
 		mainPanel.setLayout(BoxLayout.(mainPanel), BoxLayout.Y-AXIS);
 		JButton labRecordsButton = new JButton("Lab Records");
 		JButton hcButton = new JButton("View/Update Health Care Conditions");
+		hcButton.addActionListener(hcButtonListener);
+		mainPanel.add(labRecordsButton);
+		mainPanel.add(hcButton);
+		frame.add(mainPanel);
+		frame.setVisible(true);
+		}
+	public class hcButtonListener implements ActionListener{
+		void ActionPerformed(ActionEvent e){
+			Jframe hcFrame = new JFrame("Health Care Conditions");
+			UpdateHCFrame hcPanel = new UpdateHCFrame();
+		}
+	}
+	public class labButtonListener implements ActionListener{
+		void ActionPerformed(ActionEvent e){
+			LabRecords.main(args);
+		}
 	}
 }
