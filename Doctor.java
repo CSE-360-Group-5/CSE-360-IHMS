49e334182b36e091c3c023134f6b1507b37b34a8
timothymillea <timothymillea@timothys-mbp.mobile.asu.edu>
2015-10-31 14:33:07 -0700

    Added search and Java class. Further efforts will be to call main methods of update health conditions and lab records.

---
+++ Doctor.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+public class Doctor {
+	public static void main(String[] args){
+		Jframe frame = new JFrame("Doctor");
+		JPanel mainPanel = new JPanel;
+		mainPanel.setLayout(BoxLayout.(mainPanel), BoxLayout.Y-AXIS);
+		JButton labRecordsButton = new JButton("Lab Records");
+		JButton hcButton = new JButton("View/Update Health Care Conditions");
+	}
+}
