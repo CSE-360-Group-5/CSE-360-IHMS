ba4a543a48f7d438d3650797c7e5862ea931ddac
Kirik0 <mkkim6@asu.edu>
2015-11-01 14:28:31 -0700

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -8,41 +8,71 @@ public class DoctorFrame extends JPanel{
 	JButton labRecordsButton;
 	JButton hcButton;
 	JButton ePrescribeButton;
+	JButton cancel;
+	JButton save;
+	JTextField nameField;
+	JTextField DOBField;
+	JTextField prescriptionField;
+	String name;
+	String DOB;
+	String prescription;
+	String filename = "test.txt";
+	Writer writer;
+	
 	
 	public DoctorFrame() throws IOException{
 		super();
 		frame = new JFrame("Doctor");
 		mainPanel = new JPanel;
 		mainPanel.setLayout(BoxLayout.(mainPanel), BoxLayout.Y-AXIS);
+		
 		labRecordsButton = new JButton("Lab Records");
 		hcButton = new JButton("View/Update Health Care Conditions");
 		ePrescribeButton = new JButton("e-Prescribe");
+		save = new JButton("Save");
+		cancel = new JButton("Cancel");
+		
+		nameField = new JTextField(name, 20);
+		DOBField = new JTextField(DOB,20);
+		prescriptionField = new JTextField(prescription, 50);
+		
 		mainPanel.add(labRecordsButton);
 		mainPanel.add(hcButton);
 		mainPanel.add(ePrescribeButton);
+		
+		hcButtonListener hcListener = new hcButtonListener();
+		labButtonListener labListener = new labButtonListener();
+		ePrescribeButtonListener ePrescribeListener = new ePrescribeButtonListener(); 
+		SaveEPButtonListener saveEP = new SaveEPButtonListener();
+		
+		hcButton.addActionListener(hcListener);
+		labRecordsButton.addActionListener(labListener);
+		ePrescribeButton.addActionListener(ePrescribeListener);
+		
 		frame.add(mainPanel);
 		frame.setVisible(true);
 	}
-}
-
-/*
-public class Doctor {
-	public static void main(String[] args){
-		JFrame frame = new JFrame("Doctor");
-		JPanel mainPanel = new JPanel;
-		mainPanel.setLayout(BoxLayout.(mainPanel), BoxLayout.Y-AXIS);
-		JButton labRecordsButton = new JButton("Lab Records");
-		JButton hcButton = new JButton("View/Update Health Care Conditions");
-		JButton ePrescribeButton = new JButton("e-Prescribe");
-		hcButton.addActionListener(hcButtonListener);
-		labRecordsButton.addActionListener(labButtonListener);
-		ePrescribeButton.addActionListener(ePrescribeButtonListener);
-		mainPanel.add(labRecordsButton);
-		mainPanel.add(hcButton);
-		mainPanel.add(ePrescribeButton);
-		frame.add(mainPanel);
-		frame.setVisible(true);
-		}*/
+	
+	public class SaveEPButtonListener implements ActionListener{
+		void actionPerformed(ActionEvent e){
+			try{
+				writer = new BufferedWriter(new FileWriter(filename));
+				name = nameField.getText();
+				DOB = DOBField.getText();
+				prescription = prescriptionField.getText();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+			try{
+				writer.write(name+"\r\n");
+				writer.write(DOB+"\r\n");
+				writer.write(prescription);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+		
 	public class hcButtonListener implements ActionListener{
 		void ActionPerformed(ActionEvent e){
 			Jframe hcFrame = new JFrame("Health Care Conditions");
@@ -56,7 +86,20 @@ public class Doctor {
 	}
 	public class ePrescribeButtonListener implements ActionListener{
 		void ActionPerformed(ActionEvent e){
-			
+			try{
+				this.remove(labRecordsButton);
+				this.remove(hcButton);
+				this.remove(ePrescribeButton);
+				
+				this.add(nameField);
+				this.add(DOBField);
+				this.add(prescriptionField);
+				this.add(save);
+				this.add(cancel);
+				
+				save.addActionListener(saveEP);
+			}
 		}
 	}
+	
 }
