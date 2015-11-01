58b7628bcf2cb3531deeb49c3a39fa7641a324fc
Kirik0 <mkkim6@asu.edu>
2015-11-01 13:45:08 -0700

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,19 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+
+public class DoctorFrame extends JPanel{
+	JFrame frame;
+	JPanel mainPanel;
+	JButton labRecordsButton;
+	JButton hcButton;
+	JButton ePrescribeButton;
+	
+	public DoctorFrame() throws IOException{
+		super();
+		frame = new JFrame("Doctor");
+		mainPanel = new JPanel;
+		mainPanel.setLayout(BoxLayout.(mainPanel), BoxLayout.Y-AXIS);
+		labRecordsButton = new JButton("Lab Records");
+		hcButton = new JButton("View/Update Health Care Conditions");
+		ePrescribeButton = new JButton("e-Prescribe");
+		mainPanel.add(labRecordsButton);
+		mainPanel.add(hcButton);
+		mainPanel.add(ePrescribeButton);
+		frame.add(mainPanel);
+		frame.setVisible(true);
+	}
+}
+
+/*
 public class Doctor {
 	public static void main(String[] args){
-		Jframe frame = new JFrame("Doctor");
+		JFrame frame = new JFrame("Doctor");
 		JPanel mainPanel = new JPanel;
 		mainPanel.setLayout(BoxLayout.(mainPanel), BoxLayout.Y-AXIS);
 		JButton labRecordsButton = new JButton("Lab Records");
 		JButton hcButton = new JButton("View/Update Health Care Conditions");
+		JButton ePrescribeButton = new JButton("e-Prescribe");
 		hcButton.addActionListener(hcButtonListener);
+		labRecordsButton.addActionListener(labButtonListener);
+		ePrescribeButton.addActionListener(ePrescribeButtonListener);
 		mainPanel.add(labRecordsButton);
 		mainPanel.add(hcButton);
+		mainPanel.add(ePrescribeButton);
 		frame.add(mainPanel);
 		frame.setVisible(true);
-		}
+		}*/
 	public class hcButtonListener implements ActionListener{
 		void ActionPerformed(ActionEvent e){
 			Jframe hcFrame = new JFrame("Health Care Conditions");
@@ -23,6 +52,11 @@ public class Doctor {
 	public class labButtonListener implements ActionListener{
 		void ActionPerformed(ActionEvent e){
 			LabRecords.main(args);
+		}
+	}
+	public class ePrescribeButtonListener implements ActionListener{
+		void ActionPerformed(ActionEvent e){
+			
 		}
 	}
 }
