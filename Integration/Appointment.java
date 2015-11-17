feec4971b576d668d1fd24fb6e7df79bc014f661
bnamie <bnamie@asu.edu>
2015-11-16 19:15:17 -0700

    Update Appointment.java

--- Integration/Appointment.java
+++ Integration/Appointment.java
@@ -1,22 +1,15 @@
-//////////////////////////////////////////////////////////////////
-// Name: Appointment.java
-// Authors: Gareth Whitehead, Brandon Namie, Joshua Lidell
-// Description: This class defines the Appointment object
-// Date: 10/15/2015
-/////////////////////////////////////////////////////////////////
 
 public class Appointment {
-	static String doctor, month, year, date, time, type;
-	static int  row, column,patient,id;
+	String doctor, month, year, date, time;
+	int  row, column,patient,id;
 	static boolean fin;
-	public Appointment(String d, String m, String y, String da, String t, String docType, int r, int col, int p,int i){
+	public Appointment(String d, String m, String y, String da, String t, int r, int col, int p,int i){
 		patient=p;
 		doctor=d;
 		month=m;
 		year=y;
 		date=da;
 		time=t;
-		type = docType;
 		row = r;
 		id=i;
 		column = col;
@@ -48,26 +41,18 @@ public class Appointment {
 		//String ti = Integer.toString(time);
 		return time;
 	}
-	public int getRow(){
-		return row;
+	public String getRow(){
+		String r = Integer.toString(row);
+		return r;
 	}
-	public int getColumn(){
-		return column;
+	public String getColumn(){
+		String col = Integer.toString(column);
+		return col;
 	}
 	public boolean getFin(){
 		return fin;
 	}
 	public void setFin(boolean f){
 		fin=f;
-	}
-	public String toStringPatient()
-	{
-		String result = " * " + month + "/" + date + "/" + year + " - at " + time + " with Dr. " + doctor + "\n";
-		return result;
-	}
-	public String toStringDoctor()
-	{
-		String result = " * " + month + "/" + date + "/" + year + " - at " + time + "\n";
-		return result;
 	}
 }
